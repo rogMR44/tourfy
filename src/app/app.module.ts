af636e60bcdeb23233f90186897027b90673bb6b
SHIFT22 <rran2229@gmail.com>
2019-04-17 12:46:57 -0500

    Tourfy deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VisitaTecoComponent } from './visita-teco/visita-teco.component';
+import { AboutComponent } from './about/about.component';
+import { Platillo1Component } from './platillo1/platillo1.component';
+import { Platillo2Component } from './platillo2/platillo2.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VisitaTecoComponent,
+    AboutComponent,
+    Platillo1Component,
+    Platillo2Component,
+    NavComponent
   ],
   imports: [
     BrowserModule,
